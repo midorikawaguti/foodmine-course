7e81a982aff43b39775b7a3f24462e1517d713e2
Tatiana Kawaguti <tuemurak@my.centennialcollege.ca>
2023-11-16 09:55:51 -0500

    worked on services/cart

---
+++ frontend/src/app/services/cart.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { Cart } from '../shared/models/cart';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Food } from '../shared/models/Food';
+import { CartItem } from '../shared/models/cartItems';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  private cart: Cart = this.getCartFromLocalStorage();
+  private cartSubject: BehaviorSubject<Cart> = new BehaviorSubject(this.cart);
+
+  constructor() { }
+
+  addToCart(food:Food):void{
+    let cartItems = this.cart.items.find(item => item.food.id === food.id)
+    if(cartItems)
+    return;
+
+    this.cart.items.push(new CartItem(food));
+  }
+
+
+  removeFromCart(foodId: string): void{
+    this.cart.items = this.cart.items.filter(item => item.food.id ! = foodId);
+
+  }
+
+
+  changeQuantity(foodId: string, quantity:number){
+    let cartItems = this.cart.items.find(item => item.food.id === foodId);
+    if(!cartItems) return;
+
+    cartItems.quantity = quantity;
+    cartItems.price = quantity*cartItems.food.price;
+  }
+
+
+  clearCart(){
+    this.cart = new Cart();
+  }
+
+
+  getCartObservable(): Observable<Cart>{
+    return this.cartSubject.asObservable();
+  }
+
+
+  private setCartToLocalStorage():void{
+    this.cart.totalPrice = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.price,0);
+    this.cart.totalCount = this.cart.items.reduce((prevSum, currentItem) => prevSum + currentItem.quantity, 0)
+
+    const cartJson = JSON.stringify(this.cart);
+    localStorage.setItem('Cart', cartJson);
+    this.cartSubject.next(this.cart);
+  }
+
+  private getCartFromLocalStorage(): Cart{
+    const cartJson = localStorage.getItem('Cart');
+    return cartJson? JSON.parse(cartJson):new Cart();
+  }
+
+}
